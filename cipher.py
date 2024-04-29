0914b930bfbbc7a26969182b30bbed7d387f3ade
sarmednazir786 <69439476+sarmednazir786@users.noreply.github.com>
2024-04-29 21:42:38 +0000

    Add files via upload

---
+++ cipher.py
@@ -0,0 +1,25 @@
+def encrypt(key, msg):
+    encryped = []
+    for i, c in enumerate(msg):
+        key_c = ord(key[i % len(key)])
+        msg_c = ord(c)
+        encryped.append(chr((msg_c + key_c) % 127))
+    return ''.join(encryped)
+
+def decrypt(key, encryped):
+    msg = []
+    for i, c in enumerate(encryped):
+        key_c = ord(key[i % len(key)])
+        enc_c = ord(c)
+        msg.append(chr((enc_c - key_c) % 127))
+    return ''.join(msg)
+
+if __name__ == '__main__':
+    key = 'The_key'
+    
+    print("Please enter password for admin: ")
+    user_passwd=input()
+    if "\x1dNRL[\x06qD[RD" == encrypt(key, user_passwd):
+    	print(decrypt(key, "\x1bUGG\x0cal:_KR_K\x1a:WMIZK_GNJ]"))
+    else:
+    	print("Please refrain from unauthorized access")
